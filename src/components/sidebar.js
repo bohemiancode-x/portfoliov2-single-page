baef6b0952852b97c8ef9de75224f550bc64dac9
Emmanuel Ayo <emmieaddiz@gmail.com>
2022-09-26 18:22:49 +0100

    updated resume

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -97,7 +97,7 @@ export default function Sidebar({ toggleNav, nav }) {
                     <svg onClick={(e) => toggleNav(e)} className='absolute cursor-pointer top-8 right-8 h-10 w-10' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
                     </svg>
-                    <div className='flex flex-col gap-5 mt-28 px-10 items-center'>
+                    <div className='flex flex-col gap-5 mt-20 px-10 items-center'>
                         {navitems.map((nav) => (
                                 <a onClick={(e) => toggleNav(e)} key={nav.number} className= 'font-body' href={nav.path}>
                                     {nav.text}
